98ad130903dd1fc9766a59201753607bf229ce79
Vadim Fedoseev <vadimfedoseev@MacBook-Pro-Vadim.local>
2021-10-06 13:48:46 +0300

    Company debt

--- php_interface/init.php
+++ php_interface/init.php
@@ -2,4 +2,5 @@
 
 use Bitrix\Main\Page\Asset;
 
-Asset::getInstance()->addJs("/local/js/DealMenu.js");
+Asset::getInstance()->addCss( "/local/css/global.css" );
+Asset::getInstance()->addJs("/local/js/DebtCompany.js");
