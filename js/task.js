958581e64cb3c25eed0cf4746bc6724f144aa38d
Vadim Fedoseev <vadimfedoseev@MacBook-Pro-Vadim.local>
2021-10-23 21:08:10 +0300

    Update srcipt js

--- js/task.js
+++ js/task.js
@@ -3,89 +3,71 @@ BX.ready(function () {
     let user = BX.message('USER_ID');// получим текущего пользователя
     let task = getTaskID();
 
-
-    var pageTask = "/company\/personal\/user\/" + user + "\/tasks\/"; // Переменная нужна для кнопки Добавить заявку, проверка идет на открытие этой страницыы
-
     if ( task ) { // will return true
 
         let checkExist = setInterval(function() {
 
             $('.task-detail-checklist').remove(); // убираем чек лист
-            //$('.task-detail-sidebar-item task-detail-sidebar-item-robot').remove(); // убираем Автоматизация роботы (из правой части) // х.з почему не убирает
             $('.task-detail-properties').remove(); // скрыть дополнительные поля
 
 
             let buttonBar = document.getElementById('bx-component-scope-bitrix_tasks_widget_buttonstask_1');
             let sidebar = document.getElementById('sidebar');
 
-            //document.getElementById('task-time-switcher').remove(); // Убираем время
+            document.getElementById('task-time-switcher').remove(); // Убираем время
 
             //document.querySelectorAll('task-more-button ui-btn ui-btn-light-border ui-btn-dropdown').forEach(e => e.remove());
 
-            // if ( sidebar ) {
-            //
-            //     let sidebarItems = sidebar.getElementsByClassName('task-detail-sidebar-item');
-            //
-            //     for( let i = 0; i < sidebarItems.length; i++) {
-            //
-            //         // убираем Автоматизация роботы (из правой части)
-            //         if ( sidebarItems[i].className === "task-detail-sidebar-item task-detail-sidebar-item-robot" ) {
-            //             sidebarItems[i].remove();
-            //             break;
-            //         }
-            //
-            //     }
-            //
-            // }
+            if ( sidebar ) {
 
-            // Убираем пользовательские поля
-            // if ( properties !== null ) {
-            //
-            //     for ( let i = 0; i < properties.length; i++ ) {
-            //
-            //         if ( properties[i].innerHTML === 'Номер заявки' ) {
-            //             properties[i].remove();
-            //             propertiesVal[i].remove();
-            //         }
-            //
-            //     }
-            //
-            // }
+                let sidebarItems = sidebar.getElementsByClassName('task-detail-sidebar-item');
+
+                for( let i = 0; i < sidebarItems.length; i++) {
+
+                    // убираем Автоматизация роботы (из правой части)
+                    if ( sidebarItems[i].className === "task-detail-sidebar-item task-detail-sidebar-item-robot" ) {
+                        sidebarItems[i].remove();
+                        break;
+                    }
+
+                }
+
+            }
 
 
-            // if ( buttonBar !== null ) {
-            //
-            //     clearInterval(checkExist);
-            //
-            //     let spans = buttonBar.getElementsByTagName('span');
-            //
-            //     for( let i = 0; i < spans.length; i++) {
-            //
-            //         // Убираем кнопку
-            //         if (spans[i].className === "task-view-button timer-start ui-btn ui-btn-success" ) {
-            //             spans[i].remove();
-            //         }
-            //
-            //         // Скрываем кнопку "Завершить"
-            //         if (spans[i].className === "task-view-button complete pause ui-btn ui-btn-success" ) {
-            //             spans[i].remove();
-            //         }
-            //
-            //         // remove button pause
-            //         if (spans[i].className === "task-view-button timer-pause ui-btn ui-btn-light-border" ) {
-            //             spans[i].remove();
-            //         }
-            //
-            //         // // remove ЕЩЕ
-            //         if (spans[i].className === "task-more-button ui-btn ui-btn-light-border ui-btn-dropdown" ) {
-            //             spans[i].remove();
-            //         }
-            //
-            //     }
-            //
-            //     showButtons( task, user );
-            //
-            // }
+            if ( buttonBar !== null ) {
+
+                clearInterval(checkExist);
+
+                let spans = buttonBar.getElementsByTagName('span');
+
+                for( let i = 0; i < spans.length; i++) {
+
+                    // Убираем кнопку
+                    if (spans[i].className === "task-view-button timer-start ui-btn ui-btn-success" ) {
+                        spans[i].remove();
+                    }
+
+                    // Скрываем кнопку "Завершить"
+                    if (spans[i].className === "task-view-button complete pause ui-btn ui-btn-success" ) {
+                        spans[i].remove();
+                    }
+
+                    // remove button pause
+                    if (spans[i].className === "task-view-button timer-pause ui-btn ui-btn-light-border" ) {
+                        spans[i].remove();
+                    }
+
+                    // // remove ЕЩЕ
+                    if (spans[i].className === "task-more-button ui-btn ui-btn-light-border ui-btn-dropdown" ) {
+                        spans[i].remove();
+                    }
+
+                }
+
+                showButtons( task, user );
+
+            }
         }, 1);
 
     }
